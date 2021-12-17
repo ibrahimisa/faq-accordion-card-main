dda0cb5d2c17ee612b4387e6b55afe7b0b173422
Ibrahim Isa Jajere <ibrahimisajajere@gmail.com>
2021-12-17 22:08:01 +0100

    changed event listener to be on button

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 // keep reference to previously opened accordion
 let openedAccordion = null;
 
-document.querySelectorAll(".question").forEach((question) => {
-  question.addEventListener("click", () => {
+document.querySelectorAll(".btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    // grap btn parent element to be able to toogle .active
+    // class on it which toogles .answer`s display property
+    // using the sibling selector in css
+    let accordion = btn.parentElement
     // toggle active class if the same opened accordion is clicked again
-    if (openedAccordion === question) {
-      question.classList.toggle("active");
+    if (openedAccordion === accordion) {
+      accordion.classList.toggle("active");
       return;
     }
     openedAccordion?.classList.remove("active");
-    openedAccordion = question;
-    question.classList.add("active");
+    openedAccordion = accordion;
+    accordion.classList.add("active");
   });
 });
